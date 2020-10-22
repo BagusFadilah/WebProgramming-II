18036cd575ff8436c33bc4e245ab5bce2e256a9f
BagusFadilah <bagusfadilah532@gmail.com>
2020-10-22 15:28:54 +0700

    18090008

---
+++ Folder 1/pustaka_bagus/application/controllers/kenalan.php
@@ -0,0 +1,10 @@
+<?php
+class kenalan extends CI_Controller{
+    public function index() {
+        echo "<h1> perkenalan</h1>";
+        echo "Bagus Fadilah Ahmad<br>
+                saya tinggal di Wanasari<br>
+                saya suka olahraga panjat tebing";
+    }
+
+}
---
+++ Folder 1/pustaka_bagus/application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Bagus</li>
+        <li>Nama Belakang : fadilah Ahmad</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Ds. Wanasari rt 05/01 kec.Wanasari kab.Brebes</li> </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+    <li>Tegal</li></ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Parkour</li>
+        <li>jalan ringsang</li>
+    </ul>
+</section>
